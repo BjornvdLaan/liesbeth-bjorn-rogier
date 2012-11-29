00ee1ccc1a017d460738e1e5c03886c82841be0e
Rogier Slag <rogier.slag@gmail.com>
2012-11-29 11:09:23 +0100

    Added some features

--- trunk/classes/class.Spotify.php
+++ trunk/classes/class.Spotify.php
@@ -6,10 +6,10 @@ class Spotify {
         $url = sprintf('http://ws.spotify.com/search/1/artist.json?q=%s',  rawurlencode($name));
         
         $result = json_decode(file_get_contents($url));
-        if ( !isset($result->artist[0]) ) {
+        if ( !isset($result->artists[0]) ) {
             $result = new StdClass;
-            $result->href = 'Not found';
-            $result->popularity = 'Not found';
+            $result->href = '';
+            $result->popularity = '';
             return $result;
         }
         return $result->artists[0];
@@ -17,6 +17,16 @@ class Spotify {
     
     public static function getTrack($name) {
         
+    }
+    
+    public static function getAlbums($artist_id) {
+        if ( $artist_id === '' ) {
+            return array();
+        }
+        $url = sprintf(' http://ws.spotify.com/lookup/1/.josn?uri=%s&extras=albumdetail',  $artist_id);
+        
+        $result = json_decode(file_get_contents($url));
+        return $result->albums;
     }
     
 }
