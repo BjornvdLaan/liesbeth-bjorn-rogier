be8b193ddbdd1e40c77046a9b053f6448f74b6b3
Rogier Slag <rogier.slag@gmail.com>
2012-12-13 09:31:12 +0100

    popularity + release jaar

--- trunk/classes/class.Spotify.php
+++ trunk/classes/class.Spotify.php
@@ -2,7 +2,9 @@
 
 class Spotify {
     
-    public static function getArtist($name) {
+    static public $json;
+
+        public static function getArtist($name) {
         $url = sprintf('http://ws.spotify.com/search/1/artist.json?q=%s',  rawurlencode($name));
         
         $result = json_decode(file_get_contents($url));
@@ -16,12 +18,13 @@ class Spotify {
     }
     
     public static function getTrack($name,$artistID) {
-        $url = sprintf('http://ws.spotify.com/search/1/track.json?q=%s', rawurlencode($name));
+        $url = sprintf('thtp://ws.spotify.com/search/1/track.json?q=%s', rawurlencode($name));
         
         $result = json_decode(file_get_contents($url));
         foreach($result->tracks as $track) {
             foreach ( $track->artists as $artist) {
                 if ( isset($artist->href) && $artist->href == $artistID ) {
+                    Spotify::$json = $track;
                     return $track->href;
                 }
             }
@@ -37,6 +40,13 @@ class Spotify {
         
         $result = json_decode(file_get_contents($url));
         return $result->albums;
+    }
+    
+    public static function getReleaseYear() {
+        return isset(Spotify::$json->album->released)?(Spotify::$json->album->released):NULL;
+    }
+    public static function getPopularity() {
+        return isset(Spotify::$json->popularity)?(Spotify::$json->popularity):NULL;
     }
     
 }
