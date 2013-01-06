f9b5d9cc1f0abeb56c2ed2943f82088e484080b8
Rogier Slag <rogier.slag@gmail.com>
2013-01-06 15:09:15 +0100

    Opruimen vreemde map

--- trunk/trunk/modules/IKE/views/welcome.inc.php
+++
@@ -1,6 +0,0 @@
-<h1>Dear <?=$oModuleData->data->user->getUsername()?>,</h1>
-
-<h2>Welcome to Awesomo 4000, where a simple video turns into so much more.</h2>
-
-<p>Copy the link of your music video of choice here: </p>
-<?php include('videoInput.inc.php'); ?>
